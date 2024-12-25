7480bdcf09960d99e1d35c364c2a2c33684f6ade
unknown <luigibros626@gmail.com>
2024-12-25 01:05:00 +0100

    add conditional to instantly go to playstate

--- source/Main.hx
+++ source/Main.hx
@@ -5,10 +5,11 @@ import flixel.FlxState;
 import openfl.display.Sprite;
 import states.MenuState;
 import states.Startup;
+import states.PlayState;
 
 class Main extends Sprite
 {
-    public static var initialState:Class<FlxState> = MenuState;
+    public static var initialState:Class<FlxState> = #if PLAY PlayState #else MenuState #end;
 
     public function new()
     {
