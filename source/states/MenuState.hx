11d2504a7980af741799e33f1c6daf5b1bdb3d03
despawnedd <despawnedd@gmail.com>
2024-12-25 12:49:00 +0100

    start menu: make the button change scale on click

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -44,16 +44,20 @@ class MenuState extends FlxState
     function playBtnOnMouseDown(playButton:FlxSprite)
     {
         mouseWasDownOnPlayBtn = true;
+		FlxTween.tween(playButton, {"scale.x": 0.85, "scale.y": 0.85}, 0.25, {ease: FlxEase.cubeOut});
     }
 
     function playBtnOnMouseUp(playButton:FlxSprite)
     {
         if (mouseWasDownOnPlayBtn)
         {
-            FlxTween.tween(swipe, {x: -520}, 1, {ease: FlxEase.cubeOut, onComplete: 
+			FlxTween.tween(swipe, {x: -520}, 1, {
+				ease: FlxEase.cubeOut,
+				onComplete:
                 (_) -> FlxG.switchState(new PlayState())
             });
         }
+		FlxTween.tween(playButton, {"scale.x": 1, "scale.y": 1}, 0.25, {ease: FlxEase.cubeOut});
     }
 
     function playBtnOnMouseOver(playButton:FlxSprite)
