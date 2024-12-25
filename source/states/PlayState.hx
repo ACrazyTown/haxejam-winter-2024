93c76637e73d278d2a8ec798bd84f833f638c092
despawnedd <despawnedd@gmail.com>
2024-12-25 19:15:55 +0100

    the tutorial itself

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -21,6 +21,15 @@ class PlayState extends FlxState
     var curHoldingOffsetX:Float = 0;
     var curHoldingOffsetY:Float = 0;
 
+    static inline var t_popupPath:String = "assets/images/ui/tutorial/popup-";
+    static inline var t_popupAmt:Int = 11;
+    var t_dimOverlay:FlxSprite;
+    var t_pressPrompt:FlxSprite;
+    var t_popups:FlxSprite;
+    var t_currPopup:Int = 1;
+    var t_inProgress:Bool = false;
+    var t_canSwitchPopup:Bool = false;
+
     var concept:FlxSprite;
 
     var curFish:Fish;
@@ -66,20 +75,34 @@ class PlayState extends FlxState
 
         draggableObjects.reverse();
 
-        var introFadeSprite = new FlxSprite(0, 0);
-        introFadeSprite.makeGraphic(1280, 720, FlxColor.BLACK);
-        add(introFadeSprite);
+        t_dimOverlay = new FlxSprite(0, 0);
+        t_dimOverlay.makeGraphic(1280, 720, FlxColor.BLACK);
+        add(t_dimOverlay);
         FlxG.camera.zoom = 1.2;
         FlxTween.tween(FlxG.camera, {zoom: 1}, 2, {ease: FlxEase.cubeOut});
-        FlxTween.color(introFadeSprite, 2, FlxColor.BLACK, FlxColor.TRANSPARENT, {ease: FlxEase.cubeOut});
+        FlxTween.color(
+            t_dimOverlay,
+            1,
+            FlxColor.BLACK,
+            FlxColor.fromRGB(0, 0, 0, 100),
+            {ease: FlxEase.cubeOut, onComplete: startTutorial}
+        );
 
-        startInspection();
+        // startTutorial();
+        // startInspection();
     }
 
     var clickable:Bool = false;
     override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
+
+        if (t_inProgress)
+        {
+            if (t_canSwitchPopup && FlxG.mouse.justPressed)
+                switchTPopup();
+            return;
+        }
 
         for (obj in draggableObjects)
         {
@@ -130,13 +153,109 @@ class PlayState extends FlxState
         }
     }
 
+    function getCurrPopupPath()
+    {
+        var middle = Std.string(t_currPopup);
+        if (t_currPopup < 10)
+            middle = "0" + middle;
+
+        return t_popupPath + middle + ".png";
+    }
+
+    function startTutorial(_)
+    {
+        t_inProgress = true;
+
+        t_pressPrompt = new FlxSprite(-541, 573);
+        t_pressPrompt.loadGraphic("assets/images/ui/tutorial/press.png");
+        add(t_pressPrompt);
+
+        FlxTween.tween(t_pressPrompt, {x: 0}, 1, {ease: FlxEase.cubeOut});
+
+        t_popups = new FlxSprite(0, 0);
+        t_popups.loadGraphic(getCurrPopupPath());
+        t_popups.scale.set(0, 0);
+        add(t_popups);
+
+        animatePopup();
+    }
+
+    function switchTPopup()
+    {
+        t_canSwitchPopup = false;
+
+        if (t_currPopup == t_popupAmt)
+        {
+            t_inProgress = false;
+
+            FlxTween.tween(
+                t_popups,
+                {"scale.x": 0, "scale.y": 0},
+                1,
+                {
+                    ease: FlxEase.cubeIn,
+                    onComplete: (_) -> {
+                        FlxTween.color(
+                            t_dimOverlay,
+                            1,
+                            FlxColor.fromRGB(0, 0, 0, 100),
+                            FlxColor.TRANSPARENT,
+                            {ease: FlxEase.cubeOut, onComplete: startInspection}
+                        );
+
+                        FlxTween.tween(t_pressPrompt, {x: -541}, 1, {ease: FlxEase.cubeOut});
+                    }
+                }
+            );
+        }
+        else
+        {
+            if (t_currPopup == 1)
+            {
+                FlxTween.color(
+                    t_dimOverlay,
+                    1,
+                    FlxColor.fromRGB(0, 0, 0, 100),
+                    FlxColor.TRANSPARENT,
+                    {ease: FlxEase.cubeOut}
+                );
+            }
+            else if (t_currPopup == t_popupAmt - 1)
+            {
+                FlxTween.color(
+                    t_dimOverlay,
+                    1,
+                    FlxColor.TRANSPARENT,
+                    FlxColor.fromRGB(0, 0, 0, 100),
+                    {ease: FlxEase.cubeOut}
+                );
+            }
+
+            t_currPopup++;
+            t_popups.scale.set(0, 0);
+            t_popups.loadGraphic(getCurrPopupPath());
+    
+            animatePopup();
+        }
+    }
+
+    function animatePopup()
+    {
+        FlxTween.tween(
+            t_popups,
+            {"scale.x": 1, "scale.y": 1},
+            1,
+            {ease: FlxEase.bounceOut, onComplete: (_) -> {t_canSwitchPopup = true;}}
+        );
+    }
+
     function addInteractable(obj:FlxSprite):Void
     {
         add(obj);
         draggableObjects.push(obj);
     }
 
-    function startInspection():Void
+    function startInspection(_):Void
     {
         // TODO: randomize
         maxTime = 120;
